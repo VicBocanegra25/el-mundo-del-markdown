ee0ecec2fa26616fd657e79eea44ac5873926336
VicBocanegra25 <victor.bocanegra.e@gmail.com>
2023-03-05 22:15:53 -0600

    Java changes

---
+++ first_java.js
@@ -0,0 +1,14 @@
+// Testing branches with this first java file
+
+function myName(_name){
+    console.log("Hello " + _name + " we're testing branches in git.")
+}
+
+myName("Victor")
+
+function myName2(_name){
+    console.log("Hello " + _name + " this change was made in branch main.")
+}
+let varmyName2 = "Victor";
+myName2(varmyName2)
+
